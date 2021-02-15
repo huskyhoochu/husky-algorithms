a7894d94c3fd9913e96c3edeb87557d39f50e3f0
huskyhoochu <dfg1499@gmail.com>
2021-02-15 17:35:14 +0900

    Create new-id post

--- sidebars.js
+++ sidebars.js
@@ -3,6 +3,6 @@ module.exports = {
     Intro: ['introduce'],
     Leetcode: ['ip', 'balanced', 'valid-palindrome', 'reverse-string'],
     Codeit: ['linear-search'],
-    Programmers: ['practice-test', 'crane-game', 'radio-song'],
+    Programmers: ['practice-test', 'crane-game', 'radio-song', 'new-id'],
   },
 };
